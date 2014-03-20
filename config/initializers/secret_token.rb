2da183d464233cc7ab7d8b24f8b9d3f0f6756e89
pjaspers <junkiesxl@gmail.com>
2014-03-20 20:12:41 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodz::Application.config.secret_key_base = '9c2267fcd03b7b348c2bae0b795dde640ae481e6ea1e272dea5a716bd9f6756aee094a4d4233fca04a9dd4576175eb09f31f863799513c51d5e39c437852b4cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foodz::Application.config.session_store :cookie_store, key: '_foodz_session'
