b62706c3b11ab406127c415aa2a75bd53b28b9c9
pjaspers <junkiesxl@gmail.com>
2014-03-30 23:14:13 +0200

    Adding Gmail smtp credentials.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,16 @@ Foodz::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               '10to1.be',
+    user_name:            ENV["GMAIL_USER"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true}
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
