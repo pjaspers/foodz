ef7eca32d01f9c65967c45cd9cf1735c3171c684
pjaspers <junkiesxl@gmail.com>
2014-03-31 22:58:34 +0200

    Adding old orders

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe "loading from ashes" do
+    before do
+      @hash = {"metadata"=>"ne midden fitness dalton", "created_at"=>"2011-11-04T08:22:15Z", "username"=>"Piet Jaspers"}
+      @order = Order.create(@hash)
+    end
+
+    it "sets created_at" do
+      assert_equal 2011, @order.created_at.year
+    end
+
+    it "sets username" do
+      assert_equal "Piet Jaspers", @order.username
+    end
+  end
 end
