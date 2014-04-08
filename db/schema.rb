cfcdb27b95898243ebd4a5637b26c5636e25c3a9
pjaspers <junkiesxl@gmail.com>
2014-04-08 23:05:20 +0200

    Adding extension information

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 20140320215007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "fuzzystrmatch"
+  enable_extension "pg_trgm"
 
   create_table "categories", force: true do |t|
     t.string   "name"
