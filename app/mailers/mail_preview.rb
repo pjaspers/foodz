0ca8a71a5bf519804ab07188f704031670fcc6b0
pjaspers <junkiesxl@gmail.com>
2014-04-09 00:06:52 +0200

    Limiting numer of orders to show in preview.

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -1,7 +1,7 @@
 class MailPreview < MailView
   def place_orders
     delivery = Struct.new(:email).new('iets@iets.com')
-    orders = Order.all
+    orders = Order.all.limit(10)
     OrderMailer.place_orders(orders, delivery)
   end
 end
