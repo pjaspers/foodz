764054a98a1929adea317755644c75015da95161
Bob Van Landuyt <bob@vanlanduyt.co>
2014-08-04 23:41:14 +0200

    Link scope ordered on and today to eachother
    Seemed more logical since they share the logic of finding the beginning
    and ending of a day

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -15,4 +15,18 @@ class OrderTest < ActiveSupport::TestCase
       assert_equal "Piet Jaspers", @order.username
     end
   end
+
+  describe "scopes" do
+
+    it "finds old orders" do
+      o = Order.create({"metadata"=>"ne midden fitness dalton", "created_at"=>"2011-11-04T08:22:15Z", "username"=>"Piet Jaspers"})
+      assert Order.ordered_on(Date.new(2011, 11, 04)).include? o
+    end
+
+    it "finds orders for today" do
+      Order.expects(:ordered_on).with(Date.today.strftime("%Y-%m-%d")).once
+      Order.today
+    end
+
+  end
 end
